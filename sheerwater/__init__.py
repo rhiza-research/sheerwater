3cc30bea72559358796c0beaaf7bed54d6562d5d
Joshua Adkins <josh@rhizaresearch.org>
2025-12-03 23:12:44 -0800

    remove redundant config

--- sheerwater/__init__.py
+++ sheerwater/__init__.py
@@ -1,14 +1,1 @@
 """Sheerwater Benchmarking package."""
-
-# Top level config for nuthatch
-# Must be in a python file to work with coiled
-from nuthatch import set_parameter
-
-set_parameter({
-    'filesystem': "gs://sheerwater-datalake/caches",
-    'driver': "postgresql",
-    'host': "postgres.sheerwater.rhizaresearch.org",
-    'port': 5432,
-    'username': "write",
-    'database': "postgres"
-}, location='root')
