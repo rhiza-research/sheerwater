39c7354848a2382f3bd5a1471643e2009e84184a
Joshua Adkins <josh@rhizaresearch.org>
2025-12-03 18:32:52 -0800

    nuthatch working with set parameter

--- sheerwater/__init__.py
+++ sheerwater/__init__.py
@@ -2,9 +2,13 @@
 
 # Top level config for nuthatch
 # Must be in a python file to work with coiled
-from nuthatch import config_parameter
+from nuthatch import set_parameter
 
-@config_parameter('filesystem', location='root')
-def set_root_for_coiled():
-    """Dynamically set cache root to work on coiled."""
-    return "gs://sheerwater-datalake/caches"
+set_parameter({
+    'filesystem': "gs://sheerwater-datalake/caches",
+    'driver': "postgresql",
+    'host': "postgres.sheerwater.rhizaresearch.org",
+    'port': 5432,
+    'username': "write",
+    'database': "postgres"
+}, location='root')
