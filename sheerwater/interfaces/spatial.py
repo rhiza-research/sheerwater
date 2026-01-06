5dcbc2f473366b466b76b5e51c15675206569a79
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-01-05 18:50:18 -0800

    updgraded the nuthatch package

--- sheerwater/interfaces/spatial.py
+++ sheerwater/interfaces/spatial.py
@@ -48,6 +48,7 @@ class spatial(NuthatchProcessor):
         return args, kwargs
 
     def post_process(self, ds):
+        print("Post-processing")
         """Post-process the dataset to clip to the region and apply the mask."""
         if isinstance(ds, xr.Dataset):
             # Clip to specified region
