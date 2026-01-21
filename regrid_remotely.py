5e14df4f78fbceb999786789670cf5d7f6f70b09
Mohini Bariya <mohini@berkeley.edu>
2026-01-20 18:00:23 -0800

    regrid remote

--- regrid_remotely.py
+++ regrid_remotely.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     start_remote(remote_config='xxlarge_cluster', remote_name='mohini_regrid_chirps')
 
-    ds = chirps_gridded(start_time="1998-01-01", end_time="2020-12-31",
+    ds = chirps_gridded(start_time="1998-01-01", end_time="2000-12-31",
                         grid="chirps",
                         version=version, stations=stations,
                         recompute=True, cache_mode='overwrite')
