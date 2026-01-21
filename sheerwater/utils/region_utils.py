4c91aa91d4d2a1bad8b3b7b490cfaf6c13d5906c
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-01-21 13:13:57 -0800

    refactor

--- sheerwater/utils/region_utils.py
+++ sheerwater/utils/region_utils.py
@@ -3,7 +3,7 @@
 
 The regions are defined as follows:
 - admin_level_x: Administrative level x boundaries
-    - Available admin levels: 0 (national), 1 (region), 2 (county), 3 (subcounty)
+    - Available admin levels: 1 (region), 2 (county)
 - countries
     - 242 unique countries
 - subregions:
@@ -36,20 +36,148 @@ import os
 import geopandas as gpd
 from shapely.geometry import box
 
+import dask.dataframe as dd
+
+
 from nuthatch import cache
 
 from .general_utils import load_object
 
 
 def clean_name(name):
-    """Clean a name to make matching easier."""
-    return name.lower().replace(' ', '_')
+    """Clean a name to make matching easier and replace non-English characters."""
+    import unicodedata
+
+    if name is None:
+        return 'no_region'
+    name = name.lower().replace(' ', '_').strip()
+    name = name.replace('&', 'and')
+    # Normalize unicode string to remove accents, e.g., 'são_tomé_and_príncipe' -> 'sao_tome_and_principe'
+    name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore').decode('ascii')
+    name = reconcile_country_name(name)
+    return name
+
+
+def reconcile_country_name(country_name):
+    """
+    Maps a country name variant to its standardized name.
+    Unsupported territories/regions are mapped to 'no_region'.
+
+    Args:
+        country_name: String with a country name variant
+
+    Returns:
+        str: Standardized country name or 'no_region'
+    """
+    # Define standardized mappings for recognized countries
+    standardization_map = {
+        # China variants
+        'ch-in': 'china',
+        "people's_republic_of_china": 'china',
+
+        # Cape Verde variants
+        'cabo_verde': 'cape_verde',
+
+        # Central African Republic variants
+        'central_african_rep': 'central_african_republic',
+
+        # Congo variants
+        'congo,_dem_rep_of_the': 'democratic_republic_of_the_congo',
+        'congo,_rep_of_the': 'republic_of_the_congo',
+
+        # Czech Republic variants
+        'czech_republic': 'czechia',
+
+        # Ivory Coast variants
+        "cote_d'ivoire": 'ivory_coast',
+
+        # Timor-Leste variants
+        'east_timor': 'timor-leste',
+
+        # Eswatini variants
+        'swaziland': 'eswatini',
+
+        # The Bahamas variants
+        'bahamas,_the': 'the_bahamas',
+
+        # The Gambia variants
+        'gambia,_the': 'the_gambia',
+
+        # Korea variants
+        'korea,_north': 'north_korea',
+        'korea,_south': 'south_korea',
+
+        # Macedonia variants
+        'macedonia': 'north_macedonia',
+
+        # Marshall Islands variants
+        'marshall_is': 'marshall_islands',
+
+        # Micronesia variants
+        'micronesia,_fed_states_of': 'federated_states_of_micronesia',
+
+        # Myanmar variants
+        'burma': 'myanmar',
+
+        # Solomon Islands variants
+        'solomon_is': 'solomon_islands',
+
+        # Saint variants (abbreviated)
+        'st_kitts_and_nevis': 'saint_kitts_and_nevis',
+        'st_lucia': 'saint_lucia',
+        'st_vincent_and_the_grenadines': 'saint_vincent_and_the_grenadines',
+
+        # US variants
+        'united_states': 'united_states_of_america',
+
+        # Falkland Islands variants
+        'falkland_islands_(uk)': 'falkland_islands',
+
+        # Palestine variants
+        'gaza_strip': 'palestine',
+        'west_bank': 'palestine',
+    }
+
+    # # Disputed territories and dependent territories to map to 'no_region'
+    unsupported_regions = [
+        # Disputed territories
+        'abyei', 'aksai_chin', 'ashmore_and_cartier_islands', 'demchok',
+        'dragonja', 'dramana-shakatoe', 'isla_brasilera', 'kalapani',
+        'koualou', 'liancourt_rocks', "no_man's_land", 'paracel_is',
+        'senkakus', 'siachen-saltoro', 'siachen_glacier', 'somaliland',
+        'spratly_is', 'turkish_republic_of_northern_cyprus',
+        'sanafir_and_tiran_is.'
+    ]
+    #     # Dependent territories - for now, we will leave territories as they are
+    #     'american_samoa', 'anguilla', 'aruba', 'bermuda',
+    #     'british_virgin_islands', 'cayman_islands', 'cook_islands', 'curacao',
+    #     'faroe_islands', 'french_polynesia', 'french_southern_and_antarctic_lands',
+    #     'guam', 'guernsey', 'hong_kong', 'isle_of_man', 'jersey',
+    #     'macau', 'montserrat', 'new_caledonia', 'niue', 'norfolk_island',
+    #     'northern_mariana_islands', 'pitcairn_islands', 'puerto_rico',
+    #     'saint_barthelemy', 'saint_helena', 'saint_martin', 'saint_pierre_and_miquelon',
+    #     'sint_maarten', 'turks_and_caicos_islands', 'united_states_virgin_islands',
+    #     'wallis_and_futuna', 'aland', 'british_indian_ocean_territory',
+    #     'heard_island_and_mcdonald_islands', 'australian_indian_ocean_territories',
+    #     'south_georgia_and_the_south_sandwich_islands'
+    # }
+
+    # First check if it has a standardization mapping
+    if country_name in standardization_map:
+        return standardization_map[country_name]
+    # Then check if it's explicitly unsupported
+    elif country_name in unsupported_regions:
+        return 'no_region'
+    # Otherwise keep as-is (assumed to be a recognized country)
+    else:
+        return country_name
 
 
 # A set of standard regions that are above the nationional level - defined by the UN or WB
-super_national_regions = ['country', 'continent', 'subregion', 'region_un', 'region_wb']
+super_national_regions = ['continent', 'subregion', 'region_un', 'region_wb']
 # A set of standard regions that are below the nationional level - defined by the admin level
-sub_national_regions = ['admin_level_1', 'admin_level_2']
+# admin level 0 is the same as country, but we include it for consistency
+admin_level_regions = ['admin_level_0', 'admin_level_1', 'admin_level_2']
 
 # Additionally, allow the construction of custom regions by country list or lat / lon bounding box
 custom_regions = {
@@ -58,7 +186,7 @@ custom_regions = {
             'countries': ['kenya', 'burundi', 'rwanda', 'tanzania', 'uganda'],
         },
         'nimbus_west_africa': {
-            'countries': ['benin', 'burkina_faso', 'cape_verde', 'ivory_coast', 'the_gambia', 'ghana', 'guinea', 'guinea-bissau', 'liberia', 'mali', 'mauritania', 'niger', 'nigeria', 'senegal', 'sierra_leone', 'togo'],
+            'countries': ['benin', 'burkina_faso', 'cabo_verde', 'ivory_coast', 'the_gambia', 'ghana', 'guinea', 'guinea-bissau', 'liberia', 'mali', 'mauritania', 'niger', 'nigeria', 'senegal', 'sierra_leone', 'togo'],
         },
         'conus': {
             'countries': ['united_states_of_america'],
@@ -99,7 +227,7 @@ custom_regions = {
 
 # By enabling caching this can be cached locally
 @cache()
-def country_gdf():
+def super_national_regions_gdf():
     """Get the country GeoDataFrame."""
     # World geojson downloaded from https://geojson-maps.kyd.au
     filepath = 'gs://sheerwater-public-datalake/regions/world_50m.geojson'
@@ -111,26 +239,36 @@ def country_gdf():
         country_gdf[cleaned_col] = country_gdf[col].apply(clean_name)
     return country_gdf
 
-# By enabling caching this can be cached locally
 
-
-@cache(cache_args=['admin_level'])
+@cache(cache_args=['admin_level'], backend='parquet')
 def admin_level_gdf(admin_level=2):
     """Get the admin level GeoDataFrame."""
-    # World geojson downloaded from https://geojson-maps.kyd.au
-    # filepath = f'gs://sheerwater-public-datalake/regions/ken_admin{admin_level}.geojson'
     filepath = f'gs://sheerwater-public-datalake/regions/geoBoundariesCGAZ_ADM{admin_level}.geojson'
-    admin_level_gdf = gpd.read_file(load_object(filepath))
-    admin_level_gdf['admin_name'] = admin_level_gdf['shapeName'].apply(clean_name)
-    return admin_level_gdf
+    df = gpd.read_file(load_object(filepath))
+    df['admin_name'] = df['shapeName'].apply(clean_name)
+    df = df.set_index('admin_name')
+    ddf = dd.from_pandas(df, npartitions=5)
+
+    return df
 
 
 @cache()
 def region_levels_and_labels():
     """A dictionary of region levels and their labels, as small as possible to enable fast lookup."""
-    import pdb; pdb.set_trace()
-    super_national_regions_gdf = country_gdf()
-    pass
+    labels_dict = {}
+    # Populate all admin level regions
+    for region in admin_level_regions:
+        sub = admin_level_gdf(admin_level=int(region.split('_')[-1]), recompute=True)
+        labels_dict[region] = sorted(set(sub['admin_name']))
+
+    # Add all the super national regions
+    df = super_national_regions_gdf()
+    for region in super_national_regions:
+        labels_dict[region] = sorted(set(df[region]))
+
+    import pdb
+    pdb.set_trace()
+    return labels_dict
 
 
 def get_region_level(region):
