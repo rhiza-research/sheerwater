4538760f79bbd567bb8449fe56a982bf91b2566a
Joshua Adkins <josh@rhizaresearch.org>
2026-01-27 13:21:10 -0800

    finish knust

--- sheerwater/data/__init__.py
+++ sheerwater/data/__init__.py
@@ -3,6 +3,7 @@ from .chirps import chirp_v2, chirp_v3, chirps, chirps_v2, chirps_v3
 from .ghcn import ghcn, ghcn_avg
 from .imerg import imerg, imerg_final, imerg_late
 from .tahmo import tahmo, tahmo_avg
+from .knust import knust, knust_avg
 
 # Use __all__ to define what is part of the public API.
 __all__ = [
@@ -18,4 +19,6 @@ __all__ = [
     "imerg_final",
     "tahmo",
     "tahmo_avg"
+    "knust",
+    "knust_avg"
 ]
--- sheerwater/data/knust.py
+++ sheerwater/data/knust.py
@@ -1,5 +1,6 @@
 """Get knust data."""
 import math
+from functools import partial
 
 import dask
 import dask.dataframe as dd
@@ -10,13 +11,39 @@ from nuthatch.processors import timeseries
 from sheerwater.utils import dask_remote, get_grid, get_grid_ds, roll_and_agg, snap_point_to_grid
 from sheerwater.interfaces import data as sheerwater_data, spatial
 
+
 @cache(cache_args=[])
-def knust_deployment():
-    pass
+def knust_ashanti():
+    # Open the netcdf datasets and standardize
+    ashanti = xr.open_dataset('gs://sheerwater-datalake/knust_stations/ashanti.nc', engine='h5netcdf')
+    ashanti = ashanti.swap_dims({'ncells':'station_id'})
+    ashanti = ashanti.dropna(dim='time')
+    ashanti = ashanti.reset_coords('lat')
+    ashanti = ashanti.reset_coords('lon')
+    return ashanti
+
+
+@cache(cache_args=[])
+def knust_dacciwa():
+    dacciwa = xr.open_mfdataset(['gs://sheerwater-datalake/knust_stations/dacciwa/dacciwa_rg_daily_2015-2017.nc',
+                                 'gs://sheerwater-datalake/knust_stations/dacciwa/dacciwa_rg_daily_2018-2019.nc'], engine='h5netcdf')
+    dacciwa = dacciwa.rename({'station': 'station_id', 'stat_lat': 'lat', 'stat_lon': 'lon'})
+    dacciwa['station_id'] = dacciwa['station_id'].astype(str)
+    dacciwa['lat'] = dacciwa.lat.isel(time=0)
+    dacciwa['lon'] = dacciwa.lon.isel(time=0)
+    return dacciwa
+
+
+@cache(cache_args=[])
+def knust_furiflood():
+    furiflood = xr.open_dataset('gs://sheerwater-datalake/knust_stations/furiflood_kumasi_raingauge_data/furiflood_rg_01-D.nc', engine='h5netcdf')
+    furiflood = furiflood.rename({'latitude': 'lat', 'longitude': 'lon'})
+    furiflood['station_id'] = furiflood['station_id'].astype(str)
+    return furiflood
+
 
 @dask_remote
 @timeseries()
-@spatial()
 @cache(cache_args=['grid', 'cell_aggregation'],
        backend_kwargs={
            'chunking': {'time': 365, 'lat': 300, 'lon': 300}
@@ -24,82 +51,55 @@ def knust_deployment():
 def knust_raw(start_time, end_time, grid='global0_25', cell_aggregation='first', mask=None, region='global'):  # noqa: ARG001
     """Get knust data from the QC controlled stations."""
 
-    # Open the netcdf datasets and standardize
-    ashanti = xr.open_dataset('gs://sheerwater-datalake/knust_stations/ashanti.nc', engine='h5netcdf')
+    ashanti = knust_ashanti()
     ashanti = ashanti.reset_coords('lat')
     ashanti = ashanti.reset_coords('lon')
-    ashanti = ashanti.swap_dims({'ncells':'station_id'})
-
-    dacciwa = xr.open_mfdataset(['gs://sheerwater-datalake/knust_stations/dacciwa/dacciwa_rg_daily_2015-2017.nc',
-                                 'gs://sheerwater-datalake/knust_stations/dacciwa/dacciwa_rg_daily_2018-2019.nc'], engine='h5netcdf')
-    dacciwa = dacciwa.rename({'station': 'station_id', 'stat_lat': 'lat', 'stat_lon': 'lon'})
-    dacciwa['station_id'] = dacciwa['station_id'].astype(str)
-
-    furiflood = xr.open_dataset('gs://sheerwater-datalake/knust_stations/furiflood_kumasi_raingauge_data/furiflood_rg_01-D.nc', engine='h5netcdf')
-    furiflood = furiflood.rename({'latitude': 'lat', 'longitude': 'lon'})
-    furiflood['station_id'] = furiflood['station_id'].astype(str)
+    dacciwa = knust_dacciwa()
+    furiflood = knust_furiflood()
 
     # combine
     ds = xr.merge([ashanti, dacciwa, furiflood])
 
-    # Round the coordinates to the nearest grid
-    lats, lons, grid_size, offset = get_grid(grid)
+    # Snap the lat/lon values to our requested grid
+    _, _, grid_size, offset = get_grid(grid)
 
-    stat = stations.rename(columns={'location_latitude': 'lat', 'location_longitude': 'lon', 'code': 'station_id'})
-    stat['lat'] = stat['lat'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
-    stat['lon'] = stat['lon'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
+    partial_func = partial(snap_point_to_grid, grid_size=grid_size, offset=offset)
+    ds['lat'] = xr.apply_ufunc(partial_func, ds['lat'].compute(), vectorize=True)
+    ds['lon'] = xr.apply_ufunc(partial_func, ds['lon'].compute(), vectorize=True)
+    ds = ds.set_coords("lat")
+    ds = ds.set_coords("lon")
 
-    stat = stat[['station_id', 'lat', 'lon']]
-    stat = stat.set_index('station_id')
-    obs = obs.join(stat, on='station_id', how='inner')
 
-    if cell_aggregation == 'first':
-        stations_to_use = obs.groupby(['lat', 'lon']).agg(station_id=('station_id', 'first'))
-        stations_to_use = stations_to_use['station_id'].unique()
-
-        obs = obs[obs['station_id'].isin(stations_to_use)]
-        obs = obs.drop(['station_id'], axis=1)
-        obs = obs.reset_index()
-        obs = obs.drop(['index'], axis=1)
-    elif cell_aggregation == 'mean':
-        obs = obs.groupby(by=['lat', 'lon', 'time']).agg(precip=('precip', 'mean'))
-        obs = obs.reset_index()
-
-    # Convert to xarray - for this to succeed obs must be a pandas dataframe
-    obs = xr.Dataset.from_dataframe(obs.set_index(['time', 'lat', 'lon']))
+    if cell_aggregation == 'mean':
+        ds = ds.groupby(['lat','lon']).mean()
+    elif cell_aggregation == 'first':
+        ds = ds.groupby(['lat','lon']).first()
+    else:
+        raise ValueError("Cell aggregation must be 'first' or 'mean'")
 
     # Return the xarray
-    return obs
+    ds = ds.chunk({'time':365, 'lat': 300, 'lon': 300})
+    return ds
 
 
 @dask_remote
 @timeseries()
 @spatial()
-@cache(cache_args=['agg_days', 'grid', 'missing_thresh', 'cell_aggregation'],
-       backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
-def knust_rolled(start_time, end_time, agg_days,
-                 grid='global0_25',
-                 missing_thresh=0.9, cell_aggregation='first', mask=None, region='global'):
-    """knust rolled and aggregated."""
-    # Get the data
+def _knust_unified(start_time, end_time, variable, agg_days,
+                   grid='global0_25', missing_thresh=0.9, cell_aggregation='first', mask=None, region='global'):
+
     ds = knust_raw(start_time, end_time, grid, cell_aggregation, mask=mask, region=region)
+    ds = ds.rename({'precipitation_amount': 'precip'})
 
     grid_ds = get_grid_ds(grid)
     ds = ds.reindex_like(grid_ds, method='nearest', tolerance=0.005)
 
-    # Roll and agg
     agg_thresh = max(math.ceil(agg_days*missing_thresh), 1)
     ds = roll_and_agg(ds, agg=agg_days, agg_col="time", agg_fn='mean', agg_thresh=agg_thresh)
-    return ds
 
-
-@dask_remote
-def _knust_unified(start_time, end_time, variable, agg_days,
-                   grid='global0_25', missing_thresh=0.9, cell_aggregation='first', mask=None, region='global'):
     if variable != 'precip':
         raise ValueError("knust only supports precip")
 
-    ds = knust_rolled(start_time, end_time, agg_days, grid, missing_thresh, cell_aggregation, mask=mask, region=region)
     ds = ds[[variable]]
 
     # Note that this is sparse
