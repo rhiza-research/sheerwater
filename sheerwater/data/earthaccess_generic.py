0ffc24e63e2fe2b9b22dfe6bec6fef524c6ca47b
Joshua Adkins <josh@rhizaresearch.org>
2026-02-06 17:11:33 -0800

    working l3 and l4 datasources

--- sheerwater/data/earthaccess_generic.py
+++ sheerwater/data/earthaccess_generic.py
@@ -1,23 +1,18 @@
 """Generic earthaccess opener."""
-import gcsfs
 import xarray as xr
-from dateutil import parser
 from nuthatch import cache
-from nuthatch.processors import timeseries
 
-from sheerwater.utils import dask_remote, regrid, roll_and_agg, get_grid_ds
 from sheerwater.utils.secrets import earthaccess_password
+from sheerwater.utils import dask_remote
 
-from sheerwater.interfaces import data as sheerwater_data, spatial
 import dask
 import os
 import earthaccess
 
 @dask_remote
-@cache(cache_args=['filename'])
-def earthaccess_single_file(filename, earthaccess_result, preprocessor=None):
-
-    KeyboardInterrupt
+@cache(cache_args=['filename', 'preprocessor_key'])
+def earthaccess_single_file(filename, earthaccess_result, preprocessor=None, preprocessor_key=None): # noqa: ARG001
+    """Download single earthaccess file."""
     os.environ["EARTHDATA_USERNAME"] = "joshua_adkins"
     os.environ["EARTHDATA_PASSWORD"] = earthaccess_password()
     earthaccess.login(strategy="environment", persist=True)
@@ -38,29 +33,37 @@ def earthaccess_single_file(filename, earthaccess_result, preprocessor=None):
 
 
 @dask_remote
-def earthaccess_dataset(start_time, end_time, shortname, preprocessor=None, open_mfdataset_kwargs={}, delayed=False):
+def earthaccess_dataset(start_time, end_time, shortname, preprocessor=None, open_mfdataset_kwargs={},
+                        delayed=False, limit=None):
     """A generic interface to an earthaccess dataset.
 
     Opens data by shortname, and processes each file with the preprocessor before opening with mfdataset.
-
-    NOT CACHED - make sure you cache the result!"""
-
+    NOT CACHED - make sure you cache the result!
+    """
     os.environ["EARTHDATA_USERNAME"] = "joshua_adkins"
     os.environ["EARTHDATA_PASSWORD"] = earthaccess_password()
     earthaccess.login(strategy="environment", persist=True)
 
     results = earthaccess.search_data(short_name=shortname, cloud_hosted=True, temporal=(start_time, end_time))
+    results = results[:limit]
 
     if len(results) == 0:
         raise RuntimeError("No data found to open during that time.")
+
+    if preprocessor:
+        preprocessor_key = preprocessor.__name__
+    else:
+        preprocessor_key=None
 
     files = []
     for result in results:
         fname = result.data_links()[0].split('/')[-1]
         if delayed:
-            files.append(dask.delayed(earthaccess_single_file)(fname, result, preprocessor=preprocessor, filepath_only=True))
+            files.append(dask.delayed(earthaccess_single_file)(fname, result, preprocessor=preprocessor,
+                                                               preprocessor_key=preprocessor_key, filepath_only=True))
         else:
-            files.append(earthaccess_single_file(fname, result, preprocessor=preprocessor, filepath_only=True))
+            files.append(earthaccess_single_file(fname, result, preprocessor=preprocessor,
+                                                 preprocessor_key=preprocessor_key, filepath_only=True))
 
     if delayed:
         files = dask.compute(*files)
--- sheerwater/data/smap.py
+++ sheerwater/data/smap.py
@@ -1,22 +1,20 @@
 """smap data product."""
-import gcsfs
-import xarray as xr
-from dateutil import parser
 from nuthatch import cache
+import pandas as pd
 from nuthatch.processors import timeseries
 
-from sheerwater.utils import dask_remote, regrid, roll_and_agg, get_grid_ds
-from sheerwater.utils.secrets import earthaccess_password
+from sheerwater.utils import dask_remote, roll_and_agg, get_grid_ds
 
 from sheerwater.interfaces import data as sheerwater_data, spatial
-import dask
-import os
 from .earthaccess_generic import earthaccess_dataset
 
 
 @dask_remote
+@spatial()
+@timeseries
 @cache(cache_args=[], backend_kwargs={'chunking': {'y': 300, 'x': 300, 'time': 365}})
 def smap_l4_raw(start_time, end_time, delayed=False):
+    """Lr raw concatenated."""
 
     def l4_preprocessor(dt):
         ds = dt['/'].to_dataset().merge(dt['/Geophysical_Data'])
@@ -32,39 +30,67 @@ def smap_l4_raw(start_time, end_time, delayed=False):
         ds = ds.drop_attrs()
         return ds
 
-    ds = earthaccess_dataset("2016-01-01 00:00:00", "2016-01-01 02:00:00", "SPL4SMGP", preprocessor=latlon_preprocessor, delayed=delayed)
-
-    #results = earthaccess.search_data(short_name=shortname, cloud_hosted=True, temporal=("2016-01-01", "2016-01-01"))
-    #single_ds = smap_single_file(results[0].data_links()[0].split('/')[-1])
-    #ds = ds.assign_coords({'lat': single_ds.lat, 'lon': single_ds.lon})
+    single_ds = earthaccess_dataset("2016-01-01 00:00:00", "2016-01-01 00:00:00", "SPL4SMGP",
+                                    preprocessor=latlon_preprocessor, delayed=delayed, limit=1)
+    ds = ds.assign_coords({'lat': single_ds.cell_lat, 'lon': single_ds.cell_lon})
 
     return ds
 
 
 @dask_remote
+@spatial()
+@timeseries
 @cache(cache_args=[], backend_kwargs={'chunking': {'y': 300, 'x': 300, 'time': 365}})
 def smap_l3_raw(start_time, end_time, delayed=False):
+    """L3 raw concatenated."""
 
     def l3_preprocessor(dt):
-        ds = ds['/Geophysical_Data']
-        ds = ds[['sm_rootzone', 'sm_surface', 'time']]
-        ds = ds.swap_dims({"phony_dim_0": "time"})
+        ds_am = dt['/Soil_Moisture_Retrieval_Data_AM'].to_dataset()
+        ds_am = ds_am[['soil_moisture', 'tb_time_seconds']]
+        ds_am = ds_am.rename_dims({'phony_dim_0': 'y', 'phony_dim_1': 'x'})
+        time = pd.Timestamp(pd.to_datetime("2000-01-01") + ds_am['tb_time_seconds'].mean().values).round(freq='1d')
+        ds_am = ds_am.drop_vars("tb_time_seconds")
+
+        ds_pm = dt['/Soil_Moisture_Retrieval_Data_PM'].to_dataset()
+        ds_pm = ds_pm[['soil_moisture_pm']]
+        ds_pm = ds_pm.rename_dims({'phony_dim_2': 'y', 'phony_dim_3': 'x'})
+
+        ds = ds_am.merge(ds_pm)
+        ds = ds.expand_dims({'time': [time]})
+        ds = ds.drop_attrs()
+
+        return ds
+
+    ds = earthaccess_dataset(start_time, end_time, "SPL3SMP_E", preprocessor=l3_preprocessor, delayed=delayed)
+
+    ds['soil_moisture'] = ds[['soil_moisture', 'soil_moisture_pm']].to_array(dim='mdim').mean(dim='mdim', skipna=True)
+    ds = ds.drop_vars(["soil_moisture_pm"])
+
+    def latlon_preprocessor(dt):
+        ds = dt['/'].to_dataset()
         ds = ds.drop_attrs()
         return ds
 
-    ds = earthaccess_dataset(start_time, endtime, "SPL3SMP_E", preprocessor=l3_preprocessor, delayed=delayed)
+    single_ds = earthaccess_dataset("2016-01-01 00:00:00", "2016-01-01 00:00:00", "SPL4SMGP",
+                                    preprocessor=latlon_preprocessor, delayed=delayed, limit=1)
+    ds = ds.assign_coords({'lat': single_ds.cell_lat, 'lon': single_ds.cell_lon})
 
     return ds
 
 
 @dask_remote
-@cache(cache_args=['grid'], backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}},
+@cache(cache_args=['grid', 'version'], backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}},
        cache_disable_if={
            'grid': 'smap'
        })
-def smap_gridded(start_time, end_time, grid='smap'):
-
-    ds = smap_raw(start_time, end_time)
+def smap_gridded(start_time, end_time, grid='smap', version='L3'):
+    """SMAP Gridded product."""
+    if version == 'L3':
+        ds = smap_l3_raw(start_time, end_time)
+    elif version == 'L4':
+        ds = smap_l4_raw(start_time, end_time)
+    else:
+        raise ValueError("Invalid smap version")
 
     # This must be run in a coiled run machine with 'package_sync_conda_extras' set to 'esmpy'
     if grid != 'smap':
@@ -81,26 +107,18 @@ def smap_gridded(start_time, end_time, grid='smap'):
 
 
 @dask_remote
-@timeseries()
-@spatial()
-@cache(cache_args=['grid', 'agg_days'],
+@sheerwater_data()
+@cache(cache=False, cache_args=['variable', 'agg_days', 'grid', 'mask', 'region'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
-def smap_rolled(start_time, end_time, agg_days, grid, mask=None, region='global'):
-    """smap rolled and aggregated."""
-    ds = smap_gridded(start_time, end_time, grid, mask=mask, region=region)
+def smap(start_time=None, end_time=None, variable='soil_moisture', agg_days=1,
+          grid='smap', mask='lsm', region='global'): # noqa: ARG001
+    """Alias for smap final."""
+    if variable not in ['soil_moisture']:
+        raise NotImplementedError("Only soil moisture and derived variables provided by smap.")
+
+    ds = smap_gridded(start_time, end_time, grid=grid)
 
     if agg_days:
         ds = roll_and_agg(ds, agg=agg_days, agg_col="time", agg_fn='mean')
 
     return ds
-
-@dask_remote
-@sheerwater_data()
-@cache(cache=False, cache_args=['variable', 'agg_days', 'grid', 'mask', 'region'],
-       backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
-def smap(start_time=None, end_time=None, variable='precip', agg_days=1,
-          grid='global0_25', mask='lsm', region='global'):
-    """Alias for smap final."""
-    if variable not in ['precip']:
-        raise NotImplementedError("Only precip and derived variables provided by smap.")
-    return smap_rolled(start_time, end_time, agg_days=agg_days, grid=grid, mask=mask, region=region)
