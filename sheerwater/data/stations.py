Fixed lat weighting and all stations - still performance error

sheerwater/data/stations.py
@@ -6,58 +6,46 @@ from nuthatch.processors import timeseries
 
 from sheerwater.utils import dask_remote, roll_and_agg
 from sheerwater.data import knust, knust_avg, tahmo, tahmo_avg, ghcn, ghcn_avg
-from sheerwater.interfaces import data as sheerwater_data, spatial
+from sheerwater.interfaces import data as sheerwater_data, spatial, get_data
 
 
 @dask_remote
 @timeseries()
 @spatial()
 @cache(cache_args=['grid', 'missing_thresh', 'cell_aggregation', 'variable'],
-       backend_kwargs={
-           'chunking': {
-               'time': 365,
-               'lat': 300,
-               'lon': 300,
-           }
-})
+       backend_kwargs={'chunking': {'time': 365, 'lat': 300, 'lon': 300, }})
 def stations_aggregated(start_time, end_time, variable,
-                        grid='global0_25', missing_thresh=0.9, cell_aggregation='first', mask=None, region='global'):
+                        grid='global0_25', missing_thresh=0.9,
+                        cell_aggregation='mean', mask=None, region='global'):
     """Aggregate station data from all station sources into a single dataset."""
-    if cell_aggregation == 'first':
-        knust_ds = knust(start_time, end_time, variable=variable, grid=grid, agg_days=1, missing_thresh=missing_thresh,
-                         mask=mask, region=region)
-        tahmo_ds = tahmo(start_time, end_time, variable=variable, grid=grid, agg_days=1, missing_thresh=missing_thresh,
-                         mask=mask, region=region)
-        ghcn_ds = ghcn(start_time, end_time, variable=variable, grid=grid, agg_days=1, missing_thresh=missing_thresh,
-                       mask=mask, region=region)
-    elif cell_aggregation == 'mean':
-        knust_ds = knust_avg(start_time, end_time, variable=variable, grid=grid, agg_days=1,
-                             missing_thresh=missing_thresh, mask=mask, region=region)
-        tahmo_ds = tahmo_avg(start_time, end_time, variable=variable, grid=grid, agg_days=1,
-                             missing_thresh=missing_thresh, mask=mask, region=region)
-        ghcn_ds = ghcn_avg(start_time, end_time, variable=variable, grid=grid, agg_days=1, missing_thresh=missing_thresh,
-                           mask=mask, region=region)
+    suffix = '_avg' if cell_aggregation == 'mean' else ''
+    sources = ['knust', 'tahmo', 'ghcn']
 
-    knust_ds = knust_ds.expand_dims(dim={"source": ["knust"]})
-    knust_ds = knust_ds.chunk({'time': 365, 'lat': 300, 'lon': 300})
-    knust_ds['precip'] = knust_ds['precip'].astype('float32')
-    tahmo_ds = tahmo_ds.expand_dims(dim={"source": ["tahmo"]})
-    ghcn_ds = ghcn_ds.expand_dims(dim={"source": ["ghcn"]})
+    # Get all the datasets
+    fns = [(source, get_data(source + suffix)) for source in sources]
+    datasets = [fn(start_time, end_time, variable=variable, grid=grid, agg_days=1,
+                   missing_thresh=missing_thresh,
+                   mask=mask, region=region)
+                .chunk({'time': 365, 'lat': 300, 'lon': 300})
+                .expand_dims(dim={"source": [source]})
+                for source, fn in fns]
 
-    # Concat the datasets
-    ds = xr.concat([knust_ds, tahmo_ds, ghcn_ds], dim='source')
+    import pdb
+    pdb.set_trace()
 
-    if cell_aggregation == 'first':
-        ds = ds.ffill(dim='source')
-        ds = ds.bfill(dim='source')
-        ds = ds.isel(source=0)
-        ds = ds.reset_coords('source', drop=True)
-    elif cell_aggregation == 'mean':
-        # TODO: It would be better to use a weighted mean based on the number of stations in each source.
-        # This is a simple mean for now.
-        ds = ds.mean(dim='source')
+    ds = xr.conct(datasets, dim='source')
 
-    return ds
+    if cell_aggregation != 'mean':
+        raise ValueError(f"Cell aggregation {cell_aggregation} not supported")
+
+    # Compute the mean, weighted by the number of stations in each source
+    weight_sum = ds[f'{variable}_count'].sum(dim='source')
+    # weight_sum = ds[[f'{source}_count' for source in sources]].to_array(dim='source').sum(dim='source')
+    ds[f'{variable}_count'] = ds[f'{variable}_count'] / weight_sum
+    import pdb
+    pdb.set_trace()
+    out = (ds[variable] * ds[f'{variable}_count']).sum(dim='source')
+    return out.to_dataset(name=variable)
 
 
 @dask_remote