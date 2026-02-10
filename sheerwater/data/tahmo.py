ed5605ea8a0dc01e7345ea379909383de6a9e39c
Joshua Adkins <josh@rhizaresearch.org>
2026-02-09 16:28:36 -0800

    ungridded tahmo

--- sheerwater/data/tahmo.py
+++ sheerwater/data/tahmo.py
@@ -64,34 +64,51 @@ def tahmo_raw(start_time, end_time, grid='global0_25', cell_aggregation='first',
     """Get tahmo data from the QC controlled stations."""
     # Get the station list
     stations = tahmo_deployment().compute()
+    stat = stations.rename(columns={'location_latitude': 'lat', 'location_longitude': 'lon', 'code': 'station_id'})
 
     obs = tahmo_raw_daily().compute()
 
-    # Round the coordinates to the nearest grid
-    lats, lons, grid_size, offset = get_grid(grid)
+    if grid != 'tahmo':
+        # Round the coordinates to the nearest grid
+        lats, lons, grid_size, offset = get_grid(grid)
 
-    stat = stations.rename(columns={'location_latitude': 'lat', 'location_longitude': 'lon', 'code': 'station_id'})
-    stat['lat'] = stat['lat'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
-    stat['lon'] = stat['lon'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
+        stat['lat'] = stat['lat'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
+        stat['lon'] = stat['lon'].apply(lambda x: snap_point_to_grid(x, grid_size, offset))
 
-    stat = stat[['station_id', 'lat', 'lon']]
-    stat = stat.set_index('station_id')
-    obs = obs.join(stat, on='station_id', how='inner')
+        stat = stat[['station_id', 'lat', 'lon']]
+        stat = stat.set_index('station_id')
+        obs = obs.join(stat, on='station_id', how='inner')
 
-    if cell_aggregation == 'first':
-        stations_to_use = obs.groupby(['lat', 'lon']).agg(station_id=('station_id', 'first'))
-        stations_to_use = stations_to_use['station_id'].unique()
+        if cell_aggregation == 'first':
+            stations_to_use = obs.groupby(['lat', 'lon']).agg(station_id=('station_id', 'first'))
+            stations_to_use = stations_to_use['station_id'].unique()
 
-        obs = obs[obs['station_id'].isin(stations_to_use)]
-        obs = obs.drop(['station_id'], axis=1)
-        obs = obs.reset_index()
-        obs = obs.drop(['index'], axis=1)
-    elif cell_aggregation == 'mean':
-        obs = obs.groupby(by=['lat', 'lon', 'time']).agg(precip=('precip', 'mean'))
-        obs = obs.reset_index()
+            obs = obs[obs['station_id'].isin(stations_to_use)]
+            obs = obs.drop(['station_id'], axis=1)
+            obs = obs.reset_index()
+            obs = obs.drop(['index'], axis=1)
+        elif cell_aggregation == 'mean':
+            obs = obs.groupby(by=['lat', 'lon', 'time']).agg(precip=('precip', 'mean'))
+            obs = obs.reset_index()
 
-    # Convert to xarray - for this to succeed obs must be a pandas dataframe
-    obs = xr.Dataset.from_dataframe(obs.set_index(['time', 'lat', 'lon']))
+        # Convert to xarray - for this to succeed obs must be a pandas dataframe
+        obs = xr.Dataset.from_dataframe(obs.set_index(['time', 'lat', 'lon']))
+
+        # Fill out the grid for clipping and masking
+        grid_ds = get_grid_ds(grid)
+        ds = ds.reindex_like(grid_ds, method='nearest', tolerance=0.005)
+    else:
+        stat = stat[['station_id', 'lat', 'lon']]
+        stat = stat.set_index('station_id')
+        obs = obs.join(stat, on='station_id', how='inner')
+
+        obs = xr.Dataset.from_dataframe(obs.set_index(['time', 'station_id']))
+        obs = obs.chunk({'time':365, 'station_id': 50})
+        obs['lat'] = obs.lat.isel(time=0)
+        obs['lon'] = obs.lon.isel(time=0)
+        obs = obs.set_coords('lat')
+        obs = obs.set_coords('lon')
+
 
     # Return the xarray
     return obs
@@ -109,8 +126,6 @@ def tahmo_rolled(start_time, end_time, agg_days,
     # Get the data
     ds = tahmo_raw(start_time, end_time, grid, cell_aggregation, mask=mask, region=region)
 
-    grid_ds = get_grid_ds(grid)
-    ds = ds.reindex_like(grid_ds, method='nearest', tolerance=0.005)
 
     # Roll and agg
     agg_thresh = max(math.ceil(agg_days*missing_thresh), 1)
@@ -138,7 +153,7 @@ def _tahmo_unified(start_time, end_time, variable, agg_days,
        cache_args=['variable', 'agg_days', 'grid', 'mask', 'region', 'missing_thresh'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
 def tahmo(start_time=None, end_time=None, variable='precip', agg_days=1,
-          grid='global0_25', mask='lsm', region='global',  # noqa: ARG001
+          grid='global0_25', mask=None, region='global',  # noqa: ARG001
           missing_thresh=0.9):
     """Standard interface for TAHMO data."""
     return _tahmo_unified(start_time, end_time, variable, agg_days,
@@ -152,7 +167,7 @@ def tahmo(start_time=None, end_time=None, variable='precip', agg_days=1,
        cache_args=['variable', 'agg_days', 'grid', 'mask', 'region', 'missing_thresh'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
 def tahmo_avg(start_time=None, end_time=None, variable='precip', agg_days=1,
-              grid='global0_25', mask='lsm', region='global',  # noqa: ARG001
+              grid='global0_25', mask=None, region='global',  # noqa: ARG001
               missing_thresh=0.9):
     """Standard interface for TAHMO data."""
     return _tahmo_unified(start_time, end_time, variable, agg_days,
