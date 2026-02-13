e1b5be84ccb166b09385cb2c4f7e2729d817c72c
Mohini Bariya <mohini@berkeley.edu>
2026-02-13 12:16:34 -0800

    produces some low aucs that look ok?

--- test_auc.py
+++ test_auc.py
@@ -1,19 +1,23 @@
 # call the auc 
 from sheerwater.metrics import auc_roc
 from sheerwater.utils import start_remote
+from sheerwater.interfaces import list_data
 
 if __name__ == "__main__":
-    start_remote()
+    start_remote(remote_name="get-auc")
     start_time = "2024-01-01"
     end_time = "2024-12-31"
     satellite = "imerg"
-    station = "ghcn_avg"
-    station_threshold = 20
+    station = "stations"
+    station_threshold = "climatology_stations_2015_2025"
     agg_days = 5
     time_grouping = None
     space_grouping = "country"
     grid = 'global1_5'
+    region = "africa"
+    spatial = False
+
 
     auc_roc = auc_roc(start_time, end_time, satellite, station, station_threshold=station_threshold, agg_days=agg_days,
-    time_grouping=time_grouping, space_grouping=space_grouping, grid='global1_5', mask='lsm', region='global',spatial=True)
+    time_grouping=time_grouping, space_grouping=space_grouping, grid=grid, mask='lsm', region=region, spatial=spatial)
     import pdb; pdb.set_trace()
