32ebbc8f455d90625ff4bb16a789d2e24051cc0e
Mohini Bariya <mohini@berkeley.edu>
2026-02-13 13:47:15 -0800

    how to run auc

--- test_auc.py
+++ test_auc.py
@@ -1,23 +1,28 @@
 # call the auc 
-from sheerwater.metrics import auc_roc
+from sheerwater.metrics import auc
 from sheerwater.utils import start_remote
 from sheerwater.interfaces import list_data
 
 if __name__ == "__main__":
-    start_remote(remote_name="get-auc")
-    start_time = "2024-01-01"
-    end_time = "2024-12-31"
+    start_remote(remote_name="get-auc", remote_config="xlarge_cluster")
+
+    # time stuff
+    start_time = "1990-01-01"
+    end_time = "2020-12-31"
+    time_grouping = "month_of_year"
+    agg_days = 5
+
+    # space stuff
+    grid = 'global1_5'
+    mask = 'lsm'
+    region = "africa"
+    spatial = True
+
+    # satellite and station stuff
     satellite = "imerg"
     station = "stations"
     station_threshold = "climatology_stations_2015_2025"
-    agg_days = 5
-    time_grouping = None
-    space_grouping = "country"
-    grid = 'global1_5'
-    region = "africa"
-    spatial = False
 
-
-    auc_roc = auc_roc(start_time, end_time, satellite, station, station_threshold=station_threshold, agg_days=agg_days,
-    time_grouping=time_grouping, space_grouping=space_grouping, grid=grid, mask='lsm', region=region, spatial=spatial)
+    auc_roc = auc(start_time, end_time, satellite, station, station_threshold=station_threshold, agg_days=agg_days,
+    time_grouping=time_grouping, grid=grid, mask=mask, region=region, spatial=spatial)
     import pdb; pdb.set_trace()
