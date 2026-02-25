6efc6de3a316918d3fb4eadf4a660064237e735a
Joshua Adkins <josh@rhizaresearch.org>
2026-02-25 12:48:49 -0800

    remove duplicate passwords

--- sheerwater/utils/secrets.py
+++ sheerwater/utils/secrets.py
@@ -30,28 +30,6 @@ def earthaccess_password():
     return key
 
 
-def earthaccess_username():
-    """Get a postgres write password."""
-    client = secretmanager.SecretManagerServiceClient()
-
-    response = client.access_secret_version(
-        request={"name": "projects/750045969992/secrets/earthaccess_username/versions/latest"})
-    key = response.payload.data.decode("UTF-8")
-
-    return key
-
-
-def earthaccess_password():
-    """Get a postgres write password."""
-    client = secretmanager.SecretManagerServiceClient()
-
-    response = client.access_secret_version(
-        request={"name": "projects/750045969992/secrets/earthaccess_password/versions/latest"})
-    key = response.payload.data.decode("UTF-8")
-
-    return key
-
-
 def earth_data_hub_token():
     """Get a postgres write password."""
     client = secretmanager.SecretManagerServiceClient()
