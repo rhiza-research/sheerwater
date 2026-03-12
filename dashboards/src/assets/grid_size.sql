27e1628d703804b13f1a854eefa3bec7a5d7249b
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-03-12 16:54:19 -0700

    Added missing sql

--- dashboards/src/assets/grid_size.sql
+++ dashboards/src/assets/grid_size.sql
@@ -1,8 +1,8 @@
 --EXTERNAL:grid_size.sql
 SELECT
   CASE '${grid}'
-    WHEN 'global0_25'  THEN 0.25 / 2 + 1e-6
-    WHEN 'global1_5'   THEN 1.5 / 2  + 1e-6
-    WHEN 'global0_1'     THEN 0.1 / 2  + 1e-6
-    WHEN 'global0_05'     THEN 0.05 / 2  + 1e-6
+    WHEN 'global0_25'  THEN 0.25/2 + 1e-6
+    WHEN 'global1_5'   THEN 1.5/2  + 1e-6
+    WHEN 'global0_1'     THEN 0.1/2  + 1e-6
+    WHEN 'global0_05'     THEN 0.05/2  + 1e-6
   END AS grid_size
