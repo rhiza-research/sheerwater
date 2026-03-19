4c4186d742b38ef28e8e02db5bec2e1a14f44ec9
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-03-19 14:46:47 -0700

    Continue on events

--- sheerwater/interfaces/events.py
+++ sheerwater/interfaces/events.py
@@ -10,9 +10,13 @@ def event(name, variable=None):
     """Register an event. Default `variable` can be overridden when calling the event."""
     def decorator(func):
         def wrapped(ds, *args, **kwargs):
-            return func(ds, *args, **kwargs)
-        wrapped.variable = variable
+            ds = func(ds, *args, **kwargs)
+            # Average over ensemble members to get a single value
+            if 'member' in ds.dims:
+                ds = ds.mean(dim='member')
+            return ds
         wrapped.__wrapped__ = func
+        wrapped.variable = variable
         EVENTS_REGISTRY[name] = wrapped
         return wrapped
     return decorator
@@ -22,7 +26,29 @@ def event(name, variable=None):
 def above_threshold(ds, agg_days=10, threshold=10.0):
     """A function to calculate the above threshold of a dataset."""
     ds = roll_and_agg(ds, agg=agg_days, agg_col="time", agg_fn='mean')
-    return (ds >= threshold).astype(np.float32)
+    # Save and restore the null pattern, which is removed by the boolean operations
+    null_mask = ds.isnull()
+    ds = (ds >= threshold).astype(np.float32)
+    ds = ds.where(~null_mask, np.nan)
+    return ds
+
+
+@event('planting_suitability', variable='precip')
+def planting_suitability(ds, wet_spell_agg_days=10, dry_spell_agg_days=20,
+                         wet_spell_threshold=25.0, dry_spell_threshold=20.0):
+    """A function to calculate the planting suitability of a dataset."""
+    if 'precip' not in ds.variables:
+        raise ValueError("Dataset must have a 'precip' variable.")
+    wet_spell = above_threshold(ds, agg_days=wet_spell_agg_days, threshold=wet_spell_threshold / wet_spell_agg_days)
+    not_dry_spell = above_threshold(ds, agg_days=dry_spell_agg_days, threshold=dry_spell_threshold / dry_spell_agg_days)
+    # Shift to get wet spell followed by dry spell
+    not_dry_spell = not_dry_spell.shift(time=wet_spell_agg_days)
+    # Chop off the last  days for wet spell, which won't have a matching dry spell
+    wet_spell = wet_spell.isel(time=slice(None, -wet_spell_agg_days))
+    # Floatwise "and-ing" of the two spells together to get the planting suitability
+    import matplotlib.pyplot as plt
+    import pdb; pdb.set_trace()
+    return (wet_spell * not_dry_spell)
 
 
 def get_event_fn(name):
