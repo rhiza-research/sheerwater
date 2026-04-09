f9f1ac4f82ba907d708c9573e58e66a4307b39c1
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-04-08 17:55:21 -0700

    Revert "Update ecmwf"
    This reverts commit 1910537ba8e03e7b10f3e33ca95e99766fe3ed36.

--- sheerwater/forecasts/ecmwf_er.py
+++ sheerwater/forecasts/ecmwf_er.py
@@ -138,6 +138,7 @@ def ifs_extended_range(start_time, end_time, variable=None, forecast_type='forec
             # Don't rename variables we haven't registered/don't use
             pass
 
+
     if grid == 'global1_5':
         return ds
 
@@ -345,6 +346,44 @@ def ifs_extended_range_debiased_regrid(start_time, end_time, variable,
 
 
 @dask_remote
+@timeseries(timeseries='start_date')
+@spatial()
+@cache(cache_args=['variable', 'prob_type', 'agg_days', 'grid', 'debiased'],
+       cache_disable_if={'agg_days': [1, 7, 14]},
+       backend_kwargs={
+           'chunking': {"lat": 121, "lon": 240, "lead_time": 1,
+                        "start_date": 1000, "member": 1},
+           'chunk_by_arg': {
+               'grid': {
+                   'global0_25': {"lat": 721, "lon": 1440, "start_date": 30}
+               },
+           }
+})
+def ifs_extended_range_rolled(start_time, end_time, variable,
+                              prob_type='deterministic', agg_days=7,
+                              grid="global1_5", debiased=True, mask=None, region='global'):
+    """Standard format forecast data for aggregated ECMWF forecasts."""
+    run_type = 'perturbed' if prob_type == 'probabilistic' else 'average'
+    if debiased:
+        fn = ifs_extended_range_debiased_regrid
+        kwargs = {'margin_in_days': 6}
+    else:
+        fn = ifs_extended_range
+        kwargs = {'forecast_type': 'forecast'}
+
+    if agg_days not in [1, 7, 14]:  # not one of the precomputed time groups
+        ds = fn(start_time, end_time, variable,
+                run_type=run_type, time_group='daily', grid=grid, mask=mask, region=region, **kwargs)
+        # Get aggregated variable
+        ds[variable] = roll_and_agg(ds[variable], agg=agg_days, agg_col='lead_time', agg_fn='mean')
+    else:
+        time_group = {1: 'daily', 7: 'weekly', 14: 'biweekly'}[agg_days]
+        ds = fn(start_time, end_time, variable,
+                run_type=run_type, time_group=time_group, grid=grid, mask=mask, region=region, **kwargs)
+    return ds
+
+
+@dask_remote
 def _ecmwf_ifs_er_unified(start_time, end_time, variable, agg_days, prob_type='deterministic',
                           grid="global1_5", mask='lsm', region="global", debiased=True):  # noqa: ARG001
     """Unified API accessor for ECMWF raw and debiased forecasts."""
@@ -354,23 +393,21 @@ def _ecmwf_ifs_er_unified(start_time, end_time, variable, agg_days, prob_type='d
     forecast_start = shift_by_days(start_time, -46) if start_time is not None else None
     forecast_end = shift_by_days(end_time, 46) if end_time is not None else None
 
-    if debiased:
-        ds = ifs_extended_range_debiased_regrid(forecast_start, forecast_end, variable,
-                                                margin_in_days=6, run_type='average',
-                                                time_group='daily', grid=grid, mask=mask, region=region)
-    else:
-        ds = ifs_extended_range(forecast_start, forecast_end, variable,
-                                prob_type=prob_type, agg_days=agg_days,
-                                grid=grid, mask=mask, region=region)
-
-    # Roll over the lead-time dimension
-    ds = roll_and_agg(ds, agg=agg_days, agg_col='lead_time', agg_fn='mean')
+    ds = ifs_extended_range_rolled(forecast_start, forecast_end, variable, prob_type=prob_type,
+                                   agg_days=agg_days, grid=grid, debiased=debiased, mask=mask, region=region)
 
     # Assign probability label
     prob_label = prob_type if prob_type == 'deterministic' else 'ensemble'
     ds = ds.assign_attrs(prob_type=prob_label)
     if 'spatial_ref' in ds.variables:
         ds = ds.drop_vars('spatial_ref')
+
+    # TODO: remove this once we update ECMWF caches
+    if variable == 'precip' and agg_days in [7, 14] and \
+            not (grid == 'global1_5' and not debiased):
+        # The dataframes for un-debiased, global1_5 are already divided by days
+        print("Warning: Dividing precip by days to get daily values. Do you still want to do this?")
+        ds['precip'] /= agg_days
 
     # Rename to standard naming
     ds = ds.rename({'start_date': 'init_time', 'lead_time': 'prediction_timedelta'})
