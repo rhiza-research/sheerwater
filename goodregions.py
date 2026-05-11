64ca019170cd0a0f5566bf31e1f4fa1a24f19fba
Mohini Bariya <mohini@berkeley.edu>
2026-05-11 13:44:15 -0700

    good params for region finding

---
+++ goodregions.py
@@ -0,0 +1,34 @@
+from dashboard_data.rainfall_regions import get_rainfall_regions
+from sheerwater.utils import start_remote
+
+if __name__ == "__main__":
+    start_remote(remote_name="mohini")
+
+    grid = "global0_25"
+    mask = "lsm"
+
+    region = "nimbus_horn_and_east_africa"
+    kregions = 5
+    ea_regions = get_rainfall_regions(
+            data_source="imerg_final",
+            kregions=kregions,
+            region=region,
+            grid=grid,
+            mask=mask,
+            agg_days=1,
+            smooth_neighbors=50,
+            spatial_coherence=5.0,
+    )
+
+    region = "nimbus_west_africa"
+    kregions = 4
+    wa_regions = get_rainfall_regions(
+            data_source="imerg_final",
+            kregions=kregions,
+            region=region,
+            grid=grid,
+            mask=mask,
+            agg_days=1,
+            smooth_neighbors=50,
+            spatial_coherence=5.0,
+    )
