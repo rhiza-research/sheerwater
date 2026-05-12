8f12e8c799daec87e002fe17fa474bcf0c378547
Joshua Adkins <josh@rhizaresearch.org>
2026-05-12 15:16:13 -0700

    add processor interface

---
+++ sheerwater/interfaces/processors.py
@@ -0,0 +1,37 @@
+"""A decorator for processors definitions."""
+from functools import wraps
+
+PROCESSOR_REGISTRY = {}
+
+
+def processor():
+    """Stub decorator for processor definitions."""
+
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            name = fn.__name__.lower()
+
+            try:
+                ds = fn(*args, **kwargs)
+            except TypeError as e:
+                raise ValueError(f"Processor {name} requires missing processor_kwargs key. \n{e}") from e
+
+            # Add an attribute to the dataset to indicate the event name
+            ds = ds.assign_attrs({'processor': name})
+            return ds
+
+        key = fn.__name__.lower()
+        PROCESSOR_REGISTRY[key] = wrapper
+        return wrapper
+
+    return decorator
+
+
+def get_processor_fn(name):
+    """Get a processor function from the registry."""
+    if name is None:
+        return None
+    if name not in PROCESSOR_REGISTRY:
+        raise ValueError(f"Processor {name!r} not found.")
+    return PROCESSOR_REGISTRY[name]
