0e1a77b3dbd14cfe0a326a5c974785b3a769f70f
Mohini Bariya <mohini@berkeley.edu>
2026-05-15 16:12:24 -0700

    lint

--- sheerwater/spatial_subdivisions.py
+++ sheerwater/spatial_subdivisions.py
@@ -958,7 +958,6 @@ def rainfall_region_labels(grid='global0_25'):
 @cache(cache_args=['data_source', 'kregions', 'region', 'grid'])
 def get_rainfall_regions(data_source, kregions=5, region="africa", grid="global0_25", mask="lsm", agg_days=1, smooth_neighbors=50, spatial_coherence=0.0):
     """Cluster grid cells by precipitation climatology into kregions."""
-
     from sheerwater.climatology import climatology
     # time range of climatology (years don't matter)
     start_time, end_time = "1979-01-01", "1979-12-31"
