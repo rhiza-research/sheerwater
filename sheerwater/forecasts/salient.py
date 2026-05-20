1fb6770cf1f703609f29a797624a2ac7519b1ab0
Joshua Adkins <josh@rhizaresearch.org>
2026-05-20 16:22:39 -0700

    salient gem working

--- sheerwater/forecasts/salient.py
+++ sheerwater/forecasts/salient.py
@@ -4,7 +4,7 @@ import xarray as xr
 from nuthatch import cache
 from nuthatch.processors import timeseries
 
-from sheerwater.utils import dask_remote, get_variable, regrid, shift_by_days, roll_and_agg
+from sheerwater.utils import dask_remote, get_variable, regrid, shift_by_days
 from sheerwater.interfaces import forecast as sheerwater_forecast, spatial
 
 
@@ -116,7 +116,8 @@ def salient(start_time=None, end_time=None, variable="precip", agg_days=7, prob_
        backend_kwargs={
            'chunking': {"lat": 73, "lon": 77, "forecast_date": 4, 'lead': 7, 'sample': 200}
 })
-def salient_gem_raw(start_time, end_time, variable, grid='global0_25', mask=None, region='eastern_africa'):  # noqa: ARG001
+def salient_gem_raw(start_time, end_time, variable, grid='global0_25', # noqa: ARG001
+                    mask=None, region=None):  # noqa: ARG001
     """Salient GEM raw data."""
     # Your credentials are needed to access the store
     storage_options = dict(
@@ -132,24 +133,74 @@ def salient_gem_raw(start_time, end_time, variable, grid='global0_25', mask=None
     ds = ds[var].to_dataset()
     ds = ds.rename({var: variable})
 
-    # Regrid the data
-    ds = regrid(ds, grid, base='base180', method='conservative', region=region)
     return ds
-
 
 @dask_remote
 @timeseries(timeseries='forecast_date')
 @spatial()
-@cache(cache_args=['variable', 'grid'],
-       cache_disable_if={'agg_days': 1},
+@cache(cache_args=['variable', 'grid', 'prob_type'],
        backend_kwargs={
-           'chunking': {"lat": 20, "lon": 20, "forecast_date": 30, 'lead': 126, 'sample': 200}
-})
-def salient_gem_rolled(start_time, end_time, variable, agg_days=7, grid='global0_25',
-                       mask=None, region='eastern_africa'):
-    """Salient GEM rolled and aggregated."""
-    ds = salient_gem_raw(start_time, end_time, variable, grid=grid, mask=mask, region=region)
-    ds = roll_and_agg(ds, agg=agg_days, agg_col="lead", agg_fn="mean")
+           'chunking': {"lat": 73, "lon": 77, "forecast_date": 50, 'lead': 125}
+       },
+       cache_disable_if={
+           'prob_type': 'probabilistic',
+       })
+def salient_gem_processed(start_time, end_time, variable, grid, prob_type='deterministic',
+                          mask=None, region=None): # noqa: ARG001
+    """A cache of salient GEM that means the samples."""
+    # Get the data with the right days
+    ds = salient_gem_raw(start_time, end_time, variable, grid='global0_25', mask=mask, region=region)
+    if prob_type == 'deterministic':
+        ds = ds.mean(dim='sample')
+        ds = ds.assign_attrs(prob_type="deterministic")
+    elif prob_type == "probabilistic":
+        ds = ds.rename({'sample': 'member'})
+        # We just can't benchmark 200 members - down to 50. As a stride
+        # incase they have some underlying order (?)
+        ds = ds.isel(member=slice(0, None, 4))
+        ds = ds.assign_attrs(prob_type="ensemble")
+    else:
+        raise ValueError("Invalid probabilistic type")
+
+    if 'spatial_ref' in ds:
+        ds= ds.reset_coords('spatial_ref')
+
+    if 'number' in ds:
+        ds= ds.reset_coords('number')
+
+    if grid != 'global0_25':
+        # Regrid the data
+        ds = regrid(ds, grid, base='base180', method='conservative', region=region)
+
+    return ds
+
+@dask_remote
+@timeseries(timeseries='forecast_date')
+@spatial()
+@cache(cache_args=['variable', 'grid', 'prob_type'],
+       backend_kwargs={
+           'chunking': {"lat": 73, "lon": 77, "forecast_date": 50, 'lead': 125}
+       },
+       cache_disable_if={
+           'grid': 'global0_25',
+       })
+def salient_gem_regrid(start_time, end_time, variable, grid, prob_type='deterministic',
+                          mask=None, region=None): # noqa: ARG001
+    """A cache of salient GEM that means the samples."""
+    # Get the data with the right days
+    ds = salient_gem_processed(start_time, end_time, variable, grid='global0_25', prob_type=prob_type,
+                               mask=mask, region=region)
+
+    if 'spatial_ref' in ds:
+        ds = ds.drop_vars('spatial_ref')
+
+    if 'number' in ds:
+        ds = ds.drop_vars('number')
+
+    if grid != 'global0_25':
+        # Regrid the data
+        ds = regrid(ds, grid, base='base180', method='conservative', region=region)
+
     return ds
 
 
@@ -159,31 +210,19 @@ def salient_gem_rolled(start_time, end_time, variable, agg_days=7, grid='global0
        cache_args=['variable', 'agg_days', 'event', 'event_kwargs',
                    'lookback_source', 'densify', 'prob_type', 'grid', 'mask', 'region'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365, 'lead_time': 1, 'member': 1}})
-def salient_gem(start_time=None, end_time=None, variable="precip", agg_days=1, prob_type='deterministic',
+def salient_gem(start_time=None, end_time=None, variable="precip", agg_days=1, prob_type='deterministic', # noqa: ARG001
               event=None, event_kwargs=None,  # noqa: ARG001
               lookback_source=None, densify=False,  # noqa: ARG001
-              grid='global1_5', mask='lsm', region="eastern_africa"):  # noqa: ARG001
+              grid='global0_25', mask='lsm', region=None):  # noqa: ARG001
     """Final Salient GEM interface."""
-    if prob_type != 'deterministic':
-        raise NotImplementedError("Only deterministic forecast implemented for graphcast")
-
     # Get the data with the right days - the forecast is 126 days long, so pull before and after
     forecast_start = shift_by_days(start_time, -126) if start_time is not None else None
     forecast_end = shift_by_days(end_time, 126) if end_time is not None else None
 
-    # Get the data with the right days
-    ds = salient_gem_raw(forecast_start, forecast_end, variable, agg_days=agg_days, grid=grid, mask=mask, region=region)
-    if prob_type == 'deterministic':
-        ds = ds.mean(dim='samples')
-        ds = ds.reset_coords("samples", drop=True)
-        ds = ds.assign_attrs(prob_type="deterministic")
-    elif prob_type == "probabilistic":
-        ds = ds.rename({'samples': 'member'})
-        ds = ds.assign_attrs(prob_type="ensemble")
-    else:
-        raise ValueError("Invalid probabilistic type")
+    ds = salient_gem_regrid(forecast_start, forecast_end, variable, grid=grid,
+                            prob_type=prob_type, mask=mask, region=region)
 
     # Rename to standard naming
-    ds = ds.rename({'time': 'init_time', 'lead_time': 'prediction_timedelta'})
+    ds = ds.rename({'forecast_date': 'init_time', 'lead': 'prediction_timedelta'})
 
     return ds
