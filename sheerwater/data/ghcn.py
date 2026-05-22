16af8aaef74df09854ef85b205839334029b3f2b
Joshua Adkins <josh@rhizaresearch.org>
2026-05-22 16:12:59 -0700

    add back missing thresh where missing

--- sheerwater/data/ghcn.py
+++ sheerwater/data/ghcn.py
@@ -230,7 +230,7 @@ def _ghcn_unified(start_time, end_time, variable,
 @dask_remote
 @sheerwater_data()
 @cache(cache=False, cache_args=['variable', 'agg_days', 'event', 'event_kwargs', 'processors', 'processor_kwargs',
-                                'grid', 'mask', 'region'],
+                                'grid', 'mask', 'region', 'missing_thresh'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
 def ghcn(start_time=None, end_time=None, variable='precip', agg_days=1,  # noqa: ARG001
          event=None, event_kwargs=None,  # noqa: ARG001
--- sheerwater/data/tahmo.py
+++ sheerwater/data/tahmo.py
@@ -171,12 +171,12 @@ def _tahmo_unified(start_time, end_time, variable,
 @timeseries()
 @cache(cache=False,
        cache_args=['variable', 'agg_days', 'event', 'event_kwargs', 'processors', 'processor_kwargs',
-                   'grid', 'mask', 'region'],
+                   'grid', 'mask', 'region', 'missing_thresh'],
        backend_kwargs={'chunking': {'lat': 300, 'lon': 300, 'time': 365}})
 def tahmo(start_time=None, end_time=None, variable='precip', agg_days=1, # noqa: ARG001
           event=None, event_kwargs=None,  # noqa: ARG001
           processors=None, processor_kwargs=None,  # noqa: ARG001
-          grid='global0_25', mask='lsm', region='global'):  # noqa: ARG001
+          grid='global0_25', mask='lsm', region='global', missing_thresh=0.9):  # noqa: ARG001
     """Standard interface for TAHMO data."""
     return _tahmo_unified(start_time, end_time, variable,
                           grid=grid, cell_aggregation='first', mask=mask, region=region)
