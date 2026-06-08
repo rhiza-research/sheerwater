be422b6b1cec11357a1fd3f55ba7333f2ce49cad
Joshua Adkins <josh@rhizaresearch.org>
2026-06-07 22:54:40 -0700

    fix region grouping

--- sheerwater/advanced_metrics.py
+++ sheerwater/advanced_metrics.py
@@ -17,11 +17,11 @@ def get_seasonal_accumulation_kwargs(experiment, region):
         early_season_accumulation-14d
         ... and so forth.
     """
-    if region == 'africa_unimodal' or region == 'western_africa':
+    if region == 'africa_unimodal_season' or region == 'western_africa':
         time_grouping = 'year'
-    elif region == 'africa_bimodal' or region == 'eastern_africa':
+    elif region == 'africa_bimodal_season' or region == 'eastern_africa':
         time_grouping = 'two_seasons'
-    elif region == 'africa_unimodal_shifted':
+    elif region == 'africa_unimodal_shifted_season':
         time_grouping = 'shifted_season'
     else:
         warnings.warn(f"Region {region} not supported. Using year as time grouping.")
