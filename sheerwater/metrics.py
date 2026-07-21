fa9b51f94528a7bbed746adf9007416223992f7a
Joshua Adkins <josh@rhizaresearch.org>
2026-07-21 12:09:44 -0700

    commit changes for frisky

--- sheerwater/metrics.py
+++ sheerwater/metrics.py
@@ -78,7 +78,7 @@ def metric(start_time, end_time, variable, forecast, truth,
                                    time_grouping=time_grouping, space_grouping=space_grouping,
                                    spatial=spatial, grid=grid, mask=mask, region=region,
                                    memoize_forecast=memoize_forecast, memoize_truth=memoize_truth,
-                                   recompute=True) # hard code recompute to True to avoid caching issues
+                                   recompute=True, cache_mode='overwrite') # hard code recompute to True to avoid caching issues
 
     metric_name = data.attrs['metric_name']
     return data[[metric_name]]
@@ -114,7 +114,7 @@ def event_count(start_time, end_time, variable, forecast, truth,
                                    time_grouping=time_grouping, space_grouping=space_grouping,
                                    spatial=spatial, grid=grid, mask=mask, region=region,
                                    memoize_forecast=memoize_forecast, memoize_truth=memoize_truth,
-                                   recompute=True) # hard code recompute to True to avoid caching issues
+                                   recompute=True, cache_mode='overwrite') # hard code recompute to True to avoid caching issues
     return data[['event_count']]
 
 
