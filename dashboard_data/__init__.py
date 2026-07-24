b2fd2033b3fbd6e8510b007a60d504902bfa680c
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-07-24 09:24:58 -0700

    Updated imports

--- dashboard_data/__init__.py
+++ dashboard_data/__init__.py
@@ -2,7 +2,8 @@
 from .metrics_tables import (
     forecast_metric_table,
     ground_truth_metric_table,
-    advanced_spatial_metric_table)
+    advanced_spatial_metric_table,
+    advanced_ground_truth_spatial_metric_table)
 from .metrics_maps import forecast_metric_map, ground_truth_metric_map
 
 from .coverage_tables import coverage_table
@@ -11,6 +12,7 @@ __all__ = [
     'forecast_metric_table',
     'ground_truth_metric_table',
     'advanced_spatial_metric_table',
+    'advanced_ground_truth_spatial_metric_table',
     'forecast_metric_map',
     'ground_truth_metric_map',
     'coverage_table',
